5d1c4ac7fd269800a1d05072f199ca1b7a5360f2
James Lunt <jameslunt21@gmail.com>
2023-01-04 19:45:44 +0000

    kNN Bag of words predictor

--- bag_of_words.py
+++ bag_of_words.py
@@ -17,35 +17,43 @@ vectorizer = CountVectorizer()
 vectorizer = TfidfVectorizer(stop_words = "english", max_df=0.2)
 X = vectorizer.fit_transform(text)
 
+
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.model_selection import KFold
 from sklearn.metrics import mean_squared_error
-mean_error=[]; std_error=[]
-neighbor_range  = [5,10,15,20,25]
-for n in neighbor_range:
-    model = KNeighborsRegressor(n_neighbors=n,weights="distance").fit(X,y)
-    temp=[]
-    kf = KFold(n_splits=5)
-    for train, test in kf.split(X):
-        model.fit(X[train], y[train])
-        ypred = model.predict(X[test])
-        temp.append(mean_squared_error(y[test],ypred))
-    mean_error.append(np.array(temp).mean())
-    std_error.append(np.array(temp).std())
 
-#print(mean_error)
-#print(std_error)
-#plot
-plt.errorbar(neighbor_range,mean_error,yerr=std_error)
-plt.xlabel('NN'); plt.ylabel("Mean square error")
-plt.title("KNN Hyperparameter Cross-Validation")
-plt.show()
+neighbor_range  = [10,50,100,200]
+weight_m = ["distance","uniform"]
+for w in weight_m:
+    mean_error=[]; std_error=[]
+    for n in neighbor_range:
+        model = KNeighborsRegressor(n_neighbors=n,weights=w).fit(X,y)
+        temp=[]
+        kf = KFold(n_splits=5)
+        for train, test in kf.split(X):
+            model.fit(X[train], y[train])
+            ypred = model.predict(X[test])
+            temp.append(mean_squared_error(y[test],ypred))
+        mean_error.append(np.array(temp).mean())
+        std_error.append(np.array(temp).std())
+
+    print(mean_error)
+    print(std_error)
+    #plot
+    plt.errorbar(neighbor_range,mean_error,yerr=std_error)
+    plt.xlabel('NN'); plt.ylabel("Mean square error")
+    plt.title("KNN Hyperparameter Cross-Validation and " + w + " weighting")
+    plt.show()
 
 
-"""from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,shuffle=True)
 
-model = KNeighborsRegressor(n_neighbors=2,weights='distance').fit(X_train,y_train)
+model = KNeighborsRegressor(n_neighbors=100,weights='distance').fit(X_train,y_train)
 ypred = model.predict(X_test)
 print(mean_squared_error(y_test,ypred))
-"""
+
+from sklearn.dummy import DummyRegressor
+dummy = DummyRegressor(strategy="mean").fit(X_train, y_train)
+ypred = dummy.predict(X_test)
+print(mean_squared_error(y_test,ypred))
